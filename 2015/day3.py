7a218efc43614ae091594bdbcbd908107446b39a
Max <m.stolle@vanderstorm-ventures.com>
2025-08-22 10:37:39 +0200

    Lösung für day 3 part 2

--- 2015/day3.py
+++ 2015/day3.py
@@ -3,28 +3,43 @@ import os
 with open(os.path.join(os.path.dirname(__file__), 'data/day3.txt'),'r') as file:
     
     for line in file:
-        list_of_locations = []
-        times_of_visited_locations = {}
-        x = 0
-        y = 0
-        for direction in line:
-            
-            if direction == '^':
-                y += 1
-            elif direction == 'v':
-                y -= 1
-            elif direction == '>':
-                x += 1
-            elif direction == '<':
-                x -= 1
-            list_of_locations.append((x,y))
-        
-        for i in list_of_locations:
-            if i in times_of_visited_locations:
-                times_of_visited_locations[i] += 1
-            else:
-                times_of_visited_locations[i] = 1
+        intervalle = [2, -2]
+        num_location = 0
+        list_of_locations = set()
+       
+        for i in intervalle:
+            x = 0
+            y = 0
+              
+            for direction in line[::i]:
+                
+                if direction == '^':
+                    y += 1
+                elif direction == 'v':
+                    y -= 1
+                elif direction == '>':
+                    x += 1
+                elif direction == '<':
+                    x -= 1
+                list_of_locations.add((x,y))
+                print((x,y))       
+        print(len(list_of_locations))  
+
+
+
+
+
+
+
+
+        #times_of_visited_locations = {}
+
+        # for i in list_of_locations:
+        #     if i in times_of_visited_locations:
+        #         times_of_visited_locations[i] += 1
+        #     else:
+        #         times_of_visited_locations[i] = 1
         
 
-        count = sum(1 for i in times_of_visited_locations.values() if i >2)
-        print(count)
+        # count = sum(1 for i in times_of_visited_locations.values() if i >2)
+        # print(count)
